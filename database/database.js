fd014a78b867026d9b44ceb5deeedde984875a52
unknown <fernandomartins@netsite.com.br>
2020-12-15 10:16:25 -0300

    alterando a variavel de ambiente

--- database/database.js
+++ database/database.js
@@ -8,7 +8,13 @@ const sequelize = new Sequelize(
         host: process.env.DATABASE_HOST || 'localhost',
         dialect: 'postgres',
         quoteIdentifiers: false,
-        operatorsAliases: false
+        operatorsAliases: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
 );
 
